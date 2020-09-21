b78423c950a26aa8c5f55120229e105ccd1092d6
senepa <senepa.r@gmail.com>
2020-09-21 15:31:52 -0700

    add linvodb

---
+++ plug/linvodb/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var LinvoDB   = require('linvodb3');
+var db;
+
+var dbFile = __dirname + "doc";
+
+LinvoDB.dbPath = __dirname;
+
+exports.name = 'LinvoDB';
+
+exports.init = function (options, callback) {
+  fs.unlink(dbFile, function (err) {
+    if (err && err.code != 'ENOENT') {
+      callback(err);
+    } else {
+      db = new LinvoDB("doc",{});
+	  callback();
+    }
+  });
+};
+
+exports.insert = function (id, entry, callback) {
+  entry._id = id;
+  db.insert(entry);
+  callback();
+};
+
+exports.get = function (id, callback) {
+  db.findOne({ _id: id });
+  callback();
+};
